c718b7e054dd23baee280c2d0f3fdb657140ee80
Joel Riquelme <joel.riquelme@ug.uchile.cl>
2023-04-03 19:01:58 -0400

    Avance en testCards

---
+++ src/main/scala/gwent/cards/Cards.scala
@@ -0,0 +1,9 @@
+package cl.uchile.dcc
+package gwent.cards
+
+trait Cards {
+  val name : String
+  val cardType : String
+  
+  
+}
---
+++ src/main/scala/gwent/cards/UnitCard.scala
@@ -0,0 +1,9 @@
+package cl.uchile.dcc
+package gwent.cards
+
+class UnitCard (private val name : String,
+                private val cardType : String,
+                private val cardSubType : String)
+                private val strength : Int) extends Cards {
+
+}
