8f865065511331042642f9d603d5432b6f718cbb
Joel Riquelme <joel.riquelme@ug.uchile.cl>
2023-04-17 18:19:38 -0400

    implementación del decktest y arreglo de error de doble clase

--- src/main/scala/gwent/deck/Deck.scala
+++ src/main/scala/gwent/deck/Deck.scala
@@ -2,8 +2,12 @@ import cl.uchile.dcc.gwent.cards.Cards
 
 class Deck (private var listOfCards : List[Cards]) {
   def take() : Cards = {
-      var carta: Cards = listOfCards.head
-      listOfCards = listOfCards.tail
-      carta
+    var carta: Cards = listOfCards.head
+    listOfCards = listOfCards.tail
+    carta
+  }
+
+  def getlistOfCards(): List[Cards] = {
+    listOfCards
   }
 }
--- src/test/scala/deckTest.scala
+++ src/test/scala/deckTest.scala
@@ -1,26 +1,30 @@
-/* import cl.uchile.dcc.gwent.cards.Cards
+import cl.uchile.dcc.gwent.cards.Cards
 import cl.uchile.dcc.gwent.cards.unitCards.UnitCard
 import cl.uchile.dcc.gwent
 import munit.FunSuite
+import munit.Clue.generate
 
 
 class deckTest extends FunSuite{
   var testDeck: Deck = _
-  var listOfCards: Any = _
+  var listOfCards: List[Cards] = _
 
   override def beforeEach(context: BeforeEach): Unit = {
-    listOfCards = List(
+    listOfCards = List[Cards](
       new UnitCard("A",1,"Ab"),
       new UnitCard("B",1,"Ab"),
-      new UnitCard("C",1,"Ab"));
+      new UnitCard("C",1,"Ab"),
+    )
     testDeck = new Deck(listOfCards)
   }
   test("A deck can be create with a list of cards") {
-    assertEquals(testDeck.listOfCards, listOfCards)
+    assertEquals(testDeck.getlistOfCards(), listOfCards)
   }
   test("If you pick a card of a deck, the length of the deck decreases by 1"){
     testDeck.take()
-    assertEquals(testDeck.listOfCards.length, 2)
+    var lista = testDeck.getlistOfCards()
+    println(lista.length)
+    assertEquals(lista.length, 2)
   }
   test("You can only take the card of the top of the desk"){
     assertEquals(testDeck.take(), listOfCards.head)
@@ -29,5 +33,3 @@ class deckTest extends FunSuite{
   }
 
 }
-
- */
